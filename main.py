410d656ff1f3626134fdbab0b639735aef531f48
Willem Allan <wiskton@skiff.com>
2024-01-15 17:26:42 -0300

    set thumbnail

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
 import google_auth_oauthlib.flow
+from PIL import Image
 import random
 
 DEVELOPER_KEY = ''
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 client_secrets_file = "client_secret.json"
+video_id = 'il_BgisEi6E'
+thumbnail_name = 'teste.jpeg'
+
+list_comments = [
+    ';)',
+    ':)',
+    'gostei do video',
+    'boaaaaaaaaaaaa'
+]
 
 scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
-
 youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+
 
 def get_comments(video_id):
 
@@ -40,25 +51,12 @@ def get_video(video_id):
         print(result['statistics']['likeCount'])
 
 
-get_video('J0ZYt6zNTOM')
-get_comments('J0ZYt6zNTOM')
-
-# comentar um vídeo
-
 def auth():
     flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes)
     credentials = flow.run_console()
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, credentials=credentials)
     return youtube
 
-youtube = auth()
-
-list_comments = [
-    ';)',
-    ':)',
-    'gostei do video',
-    'boaaaaaaaaaaaa'
-]
 
 def set_comment(video_id, comment_text):
     insert_text = youtube.commentThreads().insert(
@@ -76,4 +74,21 @@ def set_comment(video_id, comment_text):
     )
     insert_text.execute()
 
-set_comment('J0ZYt6zNTOM', random.choice(list_comments))
+
+if __name__ == "__main__":
+    youtube = auth()
+
+    # pegar infos do vídeo
+    get_video(video_id)
+
+    # pegar os comentários do vídeo
+    get_comments(video_id)
+
+    # comentar um vídeo
+    set_comment(video_id, random.choice(list_comments))
+
+    # alterar a thumbnail
+    youtube.thumbnails().set(
+        videoId=video_id,
+        media_body=MediaFileUpload(thumbnail_name, mimetype='image/jpeg', resumable=True)
+    ).execute()
